2f3b665c7419a4c78d606bc3ba247be417c54140
boylee8713 <105021061@live.asia.edu.tw>
2018-01-09 16:24:16 +0800

    333

---
+++ page5.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>時文中 - 首頁</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        /* Remove the navbar's default margin-bottom and rounded borders */
+        .navbar {
+            margin-bottom: 0;
+            border-radius: 0;
+        }
+
+        /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+        .row.content {height: 450px}
+
+        /* Set gray background color and 100% height */
+        .sidenav {
+            padding-top: 20px;
+            background-color: #f1f1f1;
+            height: 100%;
+        }
+
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+
+        /* On small screens, set height to 'auto' for sidenav and grid */
+        @media screen and (max-width: 767px) {
+            .sidenav {
+                height: auto;
+                padding: 15px;
+            }
+            .row.content {height:auto;}
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">首頁</a></li>
+                <li><a href="page1.html">獲獎</a></li>
+                <li><a href="page2.html">執行計畫</a></li>
+                <li><a href="page3.html">教授課程</a></li>
+                <li><a href="page4.html">期刊論文</a></li>
+                <li><a href="page5.html">專書論文</a></li>
+                <li><a href="page6.html">指導大學生計畫</a></li>
+                <li><a href="page6.html">指導論文</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> 登出</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+            <p><a href="#"></a></p>
+            <p><a href="#"></a></p>
+            <p><a href="#"></a></p>
+        </div>
+        <div class="col-sm-8 text-left">
+
+
+            <h2 class="title">專書論文／Book Chapters </h2>
+            <p></p>
+            <div class="entry">
+                <table width="95%" border="0" cellpadding="0" cellspacing="0" class="tb_main">
+                    <tr class="odd">
+                        <td width="5%">1</td>
+                        <td>時文中(Wen-Chung Shih)，The New Faces of Organizations in the 21st Century<br> The Content-Based Face of Organizations<br>，2011.</td>
+                    </tr>
+                    <tr>
+                        <td width="5%">2</td>
+                        <td>時文中(Wen-Chung Shih)，Web 2.0: The Business Model<br> Teaching-Material Crystallization: Wiki-based Rapid Prototyping for Teaching-Material Design<br>，2008.10</td>
+                    </tr>
+                </table>
+                <p>&nbsp;</p>
+            </div>
+            <p></p>
+
+
+
+        </div>
+        <div class="col-sm-2 sidenav">
+
+        </div>
+    </div>
+</div>
+
+<footer class="container-fluid text-center">
+    <p align="center">
+        <span style="color: #ff8f02">亞洲大學資工系 時文中</span>
+    </p>
+</footer>
+
+</body>
+</html>
